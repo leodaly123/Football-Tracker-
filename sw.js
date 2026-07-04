277d2619bd6790853b3f411877310479da2a12a7
leodaly123 <Dalymeup@icloud.com>
2026-07-04 21:05:35 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'football-tracker-v1.2';
+const CACHE = 'football-tracker-v1.3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
